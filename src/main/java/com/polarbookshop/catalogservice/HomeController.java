c97934c792a4f9195297f6b377e20a1411c9f3fa
hsyang011 <hsyang011@gmail.com>
2025-01-02 11:28:42 +0900

    feat: 스프링 클라우드 콘피그 클라이언트로 설정 서버 사용

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties; // 사용자 정의 속성 액세스를 위해 생성자 오토와이어링을 통해 주입된 빈
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return polarProperties.getGreeting(); // 설정 데이터 빈에서 가져온 환영 메세지를 사용한다.
     }
 
 }
